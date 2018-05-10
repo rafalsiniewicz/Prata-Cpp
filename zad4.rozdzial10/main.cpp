32cdf434e0cb0dcabfc09bbb99622ce9fbdfd1b1
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2018-05-10 17:27:13 +0200

    Update main.cpp

--- zad4.rozdzial10/main.cpp
+++ zad4.rozdzial10/main.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include"przestrzen.h"
+using namespace SALES;
+int main()
+{
+    Sales* tab = new Sales[2];
+    double ar[4] = {1,2,3};
+    tab[0] = Sales(ar,4);
+    tab[1].setSales();
+    tab[0].showSales();
+    tab[1].showSales();
+
+    delete [] tab;
+    return 0;
+}
+
 
